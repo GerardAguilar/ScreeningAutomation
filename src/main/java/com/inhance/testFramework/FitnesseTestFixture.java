82df7472ff201947d7036516dbddba7b04a33305
GerardAguilar <gerard.aguilar.156@my.csun.edu>
2018-05-07 16:05:21 -0700

    Implemented Baseline and Current image comparison for Diffs

--- src/main/java/com/inhance/testFramework/FitnesseTestFixture.java
+++ src/main/java/com/inhance/testFramework/FitnesseTestFixture.java
@@ -1,14 +1,26 @@
 package com.inhance.testFramework;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.sikuli.script.FindFailed;
 import org.sikuli.script.ImagePath;
@@ -17,6 +29,10 @@ import org.sikuli.script.Screen;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import com.google.common.base.Function;
+
+import junit.framework.Assert;
 
 /***
  * 
@@ -34,10 +50,44 @@ public class FitnesseTestFixture {
 	public String globalAddress;
 	public String elementId;
 	public String xpath;
+	public ArrayList<String> navigationPath;
+	
+	public boolean enableScreenshot;
+	
+	public Function<WebElement,Boolean> testWebFixture;
+	public Function<String, Boolean> testSikuliFixture;
+	
+	public String imageFileLocation;
+	public String instanceStartTime;
+	
+	public boolean takingBaselineSet;
+	
+	public void setTakingBaselineSet(boolean temp) {
+		takingBaselineSet = temp;
+	}
+	
+	public boolean getTakingBaselineSet() {
+		return takingBaselineSet;
+	}
+	
+	public void setImageFileLocation(String temp) {
+		imageFileLocation = temp;
+	}
+	public String getImageFileLocation() {
+		return imageFileLocation;		
+	}
+		
+	public void setEnableScreenshot(boolean temp) {
+		enableScreenshot = temp;
+	}
+	
+	public boolean getEnableScreenshot() {
+		return enableScreenshot;
+	}
 	
 	public FitnesseTestFixture(){	
 			try {
-				initialize("http://www.yahoo.com/");
+				initialize("about:blank");
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -50,29 +100,6 @@ public class FitnesseTestFixture {
 	
 	public String getBaseUrl() {
 		return baseUrl;
-	}
-	
-	public int getValueZero() {
-		return valueZero;
-	} 
-	public void setValueZero(int zero) {
-		this.valueZero = zero;
-	}
-	
-	public int getValueTwo() {
-		return valueTwo;
-	}
-	
-	public void setValueTwo(int two) {
-		this.valueTwo = two;
-	}
-	
-	public boolean greaterThan() {
-		if(this.valueZero < this.valueTwo) {
-			return true;
-		}else {
-			return false;
-		}		
 	}
 	
 	public void setGlobalAddress(String temp) {
@@ -131,12 +158,25 @@ public class FitnesseTestFixture {
 //	    baseUrl = "http://www.google.com/";
 	    baseUrl = home;
 	    navigateByAddress(baseUrl);
-//	    driver.manage().window().maximize(); 
-	    driver.manage().window().setSize(new Dimension(1024, 768));
+	    driver.manage().window().maximize(); 
+//	    driver.manage().window().setSize(new Dimension(1024, 768));
 	    wait = new WebDriverWait(driver, 20);
 	    
 	    //make sure to right click the resources\images folder, select Build Path -> Include
 	    ImagePath.add(SimpleFramework.class.getCanonicalName()+"/images");
+	    enableScreenshot = false;
+	    navigationPath = new ArrayList<String>();
+	    instanceStartTime = (new Timestamp(System.currentTimeMillis())).getTime()+"";
+	    
+        File baselineDirectory = new File("C:\\baseline");
+        if (!baselineDirectory.exists()) {
+        	baselineDirectory.mkdirs();
+        }
+        
+        File currentDirectory = new File("C:\\current"+instanceStartTime);
+        if (!currentDirectory.exists()) {
+        	currentDirectory.mkdirs();
+        }
 	    
 	    return true;
 	}
@@ -175,14 +215,22 @@ public class FitnesseTestFixture {
 	
 	public void navigateByGlobalAddress() {
 //		System.out.println("navigateByGlobalAddress(String address): address = " + globalAddress); 
-		driver.get(globalAddress);
-		return;
+		if(globalAddress.length()>0) {
+			driver.get(globalAddress);
+		}else {
+			
+		}
 	}
 //	
 	public void clickElementByXpath() {
-		System.out.println("clickElementByXpath(String xpath): xpath = " + xpath);
-		WebElement element = driver.findElement(By.xpath(xpath));
-		element.click();
+		if(xpath.length()>0) {
+			System.out.println("clickElementByXpath(String xpath): xpath = " + xpath);
+			WebElement element = driver.findElement(By.xpath(xpath));
+			addXpathToNavigationPath(xpath);
+			element.click();			
+		}else {
+			
+		}
 	}
 //	
 //	public void clickElementById(String id) {
@@ -191,6 +239,21 @@ public class FitnesseTestFixture {
 //		element.click();		
 //	}
 	
+	public void addXpathToNavigationPath(String newXpath) {
+		//Since these xPaths would be used as file-names, we need to use a different divider, and just convert it at a later time
+		//There's risk in the re-conversion, but since it's minimal, it will be considered on a case by case basis (i.e. Just change the xpath value to not have a ^)
+//		newXpath.replace("\\", "^");
+		navigationPath.add(newXpath);
+	}
+	
+	public String getNavigationPath() {
+		String str = "";
+		for(int i=0; i<navigationPath.size(); i++) {
+			str = str+navigationPath.get(i);			
+		}
+		return str;
+	}
+
 	public void clickElementById() {
 		System.out.println("clickElementById(String id): elementId = " + elementId);
 		WebElement element = driver.findElement(By.id(elementId));
@@ -222,8 +285,170 @@ public class FitnesseTestFixture {
 //		return r;
 //	}	
 	
+	public void takeScreenshot() {
+		if(enableScreenshot) {
+			File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			Long timestamp = (new Timestamp(System.currentTimeMillis())).getTime();
+			System.out.println("Taking screenshot at: "+timestamp);
+			String navPath = getNavigationPath();
+			String newFilename = "c:\\baseline\\" + navPath + ".png";
+			String currentFilename = "c:\\current"+instanceStartTime+"\\"+ navPath +".png";
+			String currentDiffFilename = "c:\\current"+instanceStartTime+"\\"+ navPath +"_diff.png";
+			BufferedImage currentShot;
+			BufferedImage baselineShot;
+			
+			if(takingBaselineSet) {
+				try {
+					FileUtils.copyFile(scrFile, new File(newFilename));
+//					FileUtils.copyFile(scrFile, new File("c:\\tmp\\screenshot"+ timestamp +".png"));
+				} catch (IOException e) {
+					e.printStackTrace();
+//					Assert.fail();
+				}	
+			}else {
+				try {
+					FileUtils.copyFile(scrFile, new File(currentFilename));
+					currentShot = ImageIO.read(scrFile);
+					baselineShot = ImageIO.read(new File(newFilename));
+					compareBaseLineWithImmediateScreenshot(baselineShot,currentShot,currentDiffFilename);
+
+				} catch (IOException e) {
+					e.printStackTrace();
+//					Assert.fail();
+				}	
+			}
+			
+		}
+		//The below line should consistently default takeScreenshot as false
+		enableScreenshot = false;
+	}
+	
+
+	
+	public void clickElementByXpathPreparation(String xpath) {
+		WebElement el = driver.findElement(By.xpath(xpath));
+		Wait<WebElement> wait = new FluentWait<WebElement>(el)
+			    .withTimeout(6, TimeUnit.SECONDS)
+			    .pollingEvery(1, TimeUnit.SECONDS)
+			    .ignoring(NoSuchElementException.class);
+
+		initializeTestFixtureWeb(testWebFixture);
+		wait.until(testWebFixture);//automatically feeds the parameter used to initialize wait into the testFixture	
+	}
+	
+	public void initializeTestFixtureWeb(Function<WebElement,Boolean> testFixture) {	
+		testFixture = new Function<WebElement,Boolean>(){
+			public Boolean apply(WebElement el) {//shallow copy of address
+				//wait until element is available					
+				return el.isDisplayed();					
+			}
+		};			
+	}
+	
+	public boolean verifyImageBySikuli(){
+		boolean present = false;
+		if(imageFileLocation.length()>0) {			
+			Wait<String> wait = new FluentWait<String>(imageFileLocation)
+				    .withTimeout(6, TimeUnit.SECONDS)
+				    .pollingEvery(1, TimeUnit.SECONDS)
+				    .ignoring(NoSuchElementException.class);
+			
+			try {
+				initializeTestFixtureSikuli(testSikuliFixture);
+				wait.until(testSikuliFixture);
+				present = true;
+			} catch (FindFailed e) {
+//				e.printStackTrace();
+				//if we couldn't find the image, return false for verify image by sikuli, instead of just exiting out with an exception
+				present = false;
+			}					
+		}
+		return present;	
+	}
+	
+	public void initializeTestFixtureSikuli(Function<String,Boolean> testFixture) throws FindFailed{
+		final Region r = s;
+		testSikuliFixture = new Function<String,Boolean>() {
+			public Boolean apply (String imageFileLocation){
+				try {
+					if(r.find(imageFileLocation)!=null) {
+						return true;
+					}else {
+						return false;
+					}
+				} 
+				catch (FindFailed e) {
+//					e.printStackTrace();
+					try {
+						throw new FindFailed(imageFileLocation + " not found.");
+					} catch (FindFailed e1) {
+						// TODO Auto-generated catch block
+//						e1.printStackTrace();
+						return false;
+					}
+				}				
+			}	
+		};	
+	}
+	
+	//output the diff into the proper location
+	public void compareBaseLineWithImmediateScreenshot(BufferedImage baselineImage, BufferedImage newImage, String someLocation) {
+		BufferedImage diff = getDifferenceImage(baselineImage, newImage);
+		File outputfile = new File(someLocation); 
+		try {
+			ImageIO.write(diff, "png", outputfile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+		
+	//generate the diff
+	public static BufferedImage getDifferenceImage(BufferedImage img1, BufferedImage img2) {
+	    int width1 = img1.getWidth(); // Change - getWidth() and getHeight() for BufferedImage
+	    int width2 = img2.getWidth(); // take no arguments
+	    int height1 = img1.getHeight();
+	    int height2 = img2.getHeight();
+	    if ((width1 != width2) || (height1 != height2)) {
+	        System.err.println("Error: Images dimensions mismatch");
+	        System.exit(1);
+	    }
+
+	    // NEW - Create output Buffered image of type RGB
+	    BufferedImage outImg = new BufferedImage(width1, height1, BufferedImage.TYPE_INT_RGB);
+
+	    // Modified - Changed to int as pixels are ints
+	    int diff;
+	    int result; // Stores output pixel
+	    for (int i = 0; i < height1; i++) {
+	        for (int j = 0; j < width1; j++) {
+	            int rgb1 = img1.getRGB(j, i);
+	            int rgb2 = img2.getRGB(j, i);
+	            int r1 = (rgb1 >> 16) & 0xff;
+	            int g1 = (rgb1 >> 8) & 0xff;
+	            int b1 = (rgb1) & 0xff;
+	            int r2 = (rgb2 >> 16) & 0xff;
+	            int g2 = (rgb2 >> 8) & 0xff;
+	            int b2 = (rgb2) & 0xff;
+	            diff = Math.abs(r1 - r2); // Change
+	            diff += Math.abs(g1 - g2);
+	            diff += Math.abs(b1 - b2);
+	            diff /= 3; // Change - Ensure result is between 0 - 255
+	            // Make the difference image gray scale
+	            // The RGB components are all the same
+	            result = (diff << 16) | (diff << 8) | diff;
+	            outImg.setRGB(j, i, result); // Set result
+	        }
+	    }
+
+	    // Now return
+	    return outImg;
+	}
+	
 	public static void main(String[] args) {		
 		FitnesseTestFixture temp = new FitnesseTestFixture();
 	}
+	
+
+	
 	
 }
